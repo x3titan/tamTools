e31d4e1fce5cff16f86b702f57217eb6607067de
JesseTam <x3reunion@yahoo.com>
2019-04-09 17:18:34 +0800

    photo auto resize, useful in website to process user's uploaded image file.

---
+++ PhotoResize/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace PhotoResize {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 394);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(647, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(647, 394);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(639, 368);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = " 设置 ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(105, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(77, 21);
+            this.textBox4.TabIndex = 21;
+            this.textBox4.Text = "0";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "间隔执行(秒)";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(285, 83);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "START !!!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "备份目录";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "源目录";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(81, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 21);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(536, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 21);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(81, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 21);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(536, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 21);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(639, 368);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = " 日志 ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(633, 362);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 416);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "1陪驾照片压缩备份 V0.1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+    }
+}
+
---
+++ PhotoResize/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace PhotoResize {
+    public partial class Form1 : Form {
+        TamPubWin1.LogFile log = new TamPubWin1.LogFile();
+        ImageResize task = new ImageResize();
+
+        public Form1() {
+            InitializeComponent();
+            log.richTextBox = richTextBox1;
+            loadConfig();
+            timer1.Enabled = true;
+        }
+
+        public void loadConfig() {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+            textBox1.Text = TamPub1.ConfigFileXml.readString(filename, "FolderSource", "");
+            textBox2.Text = TamPub1.ConfigFileXml.readString(filename, "FolderBackup", "");
+            textBox4.Text = TamPub1.ConfigFileXml.readString(filename, "repeatInterval", "");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if (f.ShowDialog() != DialogResult.OK) return;
+            textBox2.Text = f.SelectedPath;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if (f.ShowDialog() != DialogResult.OK) return;
+            textBox1.Text = f.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if (f.ShowDialog() != DialogResult.OK) return;
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            task = new ImageResize();
+            task.folderSource = textBox1.Text;
+            task.folderBackup = textBox2.Text;
+            try {
+                task.repeatInterval = Convert.ToInt32(textBox4.Text);
+            } catch {
+                task.repeatInterval = 0;
+            }
+
+            Thread threadCopy = new Thread(new ThreadStart(task.copyFolderDiff));
+            threadCopy.Name = "threadCopy";
+            threadCopy.Priority = ThreadPriority.BelowNormal;
+            threadCopy.Start();
+        }
+
+        public class ImageResize {
+            public TamPub1.MemoryLog log = new TamPub1.MemoryLog();
+            public string folderSource = "";
+            public string folderBackup = "";
+            public string folderDiff = "";
+            private string folderName = "";
+            private string folderDiffReal = "";
+            public int backupCount = 0;
+            public int totalCount = 0;
+            public int repeatInterval = 0;
+            public string statusString = "";
+            public bool exit = false;
+            public ListView listView = null;
+
+            public ImageResize() {
+                log.size = 3000;
+            }
+
+            private void init() {
+                folderSource = TamPub1.StringFunc.deleteRearChar(folderSource.Trim(), '\\');
+                folderBackup = TamPub1.StringFunc.deleteRearChar(folderBackup.Trim(), '\\');
+                int a = folderSource.LastIndexOf("\\");
+                if (a < 0) {
+                    folderName = "";
+                    return;
+                }
+                folderName = folderSource.Substring(a + 1);
+                if (folderDiff.Length > 0) {
+                    DateTime currentTime = DateTime.Now;
+                    folderDiffReal = folderDiff + "\\" + folderName + "\\" +
+                        currentTime.ToString("yyyy_MM_dd") + "\\" +
+                        currentTime.ToString("HH_mm_ss");
+                }
+            }
+
+            public void copyFolderDiff() {
+                while (true) {
+                    init();
+                    //log.writeWarning("start backup, source=" + folderSource + ",backup=" + folderBackup + ",diff=" + folderDiff);
+                    statusString = "正在压缩图片...";
+                    backupCount = 0;
+                    totalCount = 0;
+                    try {
+                        copyFolderDiffImp(folderSource);
+                    } catch (Exception ee) {
+                        log.writeWarning("压缩过程中出现错误, error=" + ee.Message + ",trace=" + ee.StackTrace);
+                    }
+                    if (backupCount > 0) {
+                        log.writeWarning("backup complete, totalCount=" + totalCount + ",backupCount=" + backupCount);
+                    }
+                    if (repeatInterval <= 0) break;
+                    DateTime startTime = DateTime.Now;
+                    while (true) {
+                        statusString = "压缩完成，离下一次压缩还剩" + Math.Floor(repeatInterval - (DateTime.Now - startTime).TotalSeconds) + "秒";
+                        if ((DateTime.Now - startTime).TotalSeconds >= repeatInterval) break;
+                        if (exit) break;
+                        Thread.Sleep(100);
+                    }
+                    if (exit) break;
+                }
+                statusString = "图片压缩完成";
+            }
+
+            public void copyFolderDiffImp(string sfolder) {
+                if (exit) return;
+                DirectoryInfo dir = new DirectoryInfo(sfolder);
+                //不是目录
+                if (dir == null) return; 
+                FileSystemInfo[] files = dir.GetFileSystemInfos();
+                for (int i = 0; i < files.Length; i++) {
+                    FileInfo file = files[i] as FileInfo;
+                    if (file != null) { //是文件
+                        if (TamPub1.FileOperation.extractFileName(file.FullName).ToUpper().Equals("THUMBS.DB")) {
+                            continue;
+                        }
+                        string ext = System.IO.Path.GetExtension(file.FullName).ToLower();
+                        if (!ext.Equals(".jpg") &&
+                            !ext.Equals(".jpeg")) continue;
+                        string destFilename = folderBackup + "\\" + folderName + file.FullName.Substring(folderSource.Length);
+                        totalCount++;
+                        bool needCopy = false;
+                        if (!File.Exists(destFilename)) {
+                            needCopy = true;
+                        }
+                        if (needCopy) {
+                            backupCount++;
+                            System.IO.Directory.CreateDirectory(TamPub1.FileOperation.extractFilePath(destFilename));
+                            //拷贝源文件
+                            File.Copy(file.FullName, destFilename, true);
+                            //压缩文件
+                            string hdr = TamPub1.StringFunc.copy(TamPub1.FileOperation.extractFileName(file.FullName), 0, 2).ToLower();
+                            var xSize = 800;
+                            if (hdr.Equals("cb")) { //背景
+                                xSize = 800;
+                            } else if (hdr.Equals("cf")) { //头像
+                                xSize = 400;
+                            } else if (hdr.Equals("id")) { //身份证
+                                xSize = 1500;
+                            } else if (hdr.Equals("cl")) { //行驶证
+                                xSize = 1500;
+                            } else if (hdr.Equals("dl")) { //驾驶证
+                                xSize = 1500;
+                            } else if (hdr.Equals("bk")) { //副刹车
+                                xSize = 1000;
+                            } else if (hdr.Equals("dr")) { //j.行车记录仪
+                                xSize = 1000;
+                            }
+
+                            //比例缩放
+                            Image initimage;
+                            try {
+                                initimage = Image.FromFile(file.FullName);
+                            } catch {
+                                FileInfo df = new FileInfo(file.FullName);
+                                if (df.Length < 50000) {
+                                    log.writeWarning("由于图片上传未完成(" + (df.Length / 1000) + "k)或者文件格式不对，无法处理: " + file.FullName);
+                                } else {
+                                    log.writeWarning("由于图片过大(" + (df.Length / 1000) + "k)或者文件格式不对，无法处理: " + file.FullName);
+                                }
+                                continue;
+                            }
+                            int newWidth = initimage.Width;
+                            int newHeight = initimage.Height;
+                            if (newWidth > newHeight) {
+                                if (newWidth > xSize) {
+                                    newHeight = xSize * newHeight / newWidth;
+                                    newWidth = xSize;
+                                }
+                            } else {
+                                if (newHeight > xSize) {
+                                    newWidth = xSize * newWidth / newHeight;
+                                    newHeight = xSize;
+                                }
+                            }
+                            //新建一个bmp图片
+                            Image newimage = new Bitmap((int)newWidth, (int)newHeight);
+                            //新建一个画板
+                            Graphics newg = Graphics.FromImage(newimage);
+                            //设置质量
+                            newg.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                            newg.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                            //置背景色
+                            newg.Clear(Color.White);
+                            //画图
+                            newg.DrawImage(initimage, new Rectangle(0, 0, newimage.Width, newimage.Height), new Rectangle(0, 0, initimage.Width, initimage.Height), GraphicsUnit.Pixel);
+                            //保存缩略图
+                            initimage.Dispose();
+                            File.Delete(file.FullName);
+                            newimage.Save(file.FullName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            //释放资源
+                            newg.Dispose();
+                            newimage.Dispose();
+                            GC.Collect();
+                            log.writeCommon("拷贝并压缩文件: " + file.FullName);
+                        }
+                    } else {  //对于子目录
+                        dir = files[i] as DirectoryInfo;
+                        if (needProcess(dir.FullName)) {
+                            copyFolderDiffImp(dir.FullName);
+                        }
+                    }
+                }
+            }
+            private bool needProcess(string filePath) {
+                filePath = filePath.ToUpper();
+                filePath = TamPub1.StringFunc.deleteRearChar(filePath, '\\');
+                for (var i = 0; i < listView.Items.Count; i++) {
+                    if (listView.Items[i].Text.ToUpper().Equals(filePath)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            //process log
+            var count = 0;
+            log.datetimeType = 0;
+            while (task.log.pop()) {
+                log.writeLogString(task.log.current.logType, task.log.current.logString);
+                count++;
+                if (count > 100) break;
+            }
+            log.datetimeType = 10;
+
+            //process status
+            if (!toolStripStatusLabel1.Text.Equals(task.statusString)) {
+                toolStripStatusLabel1.Text = task.statusString;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+            TamPub1.ConfigFileXml.writeString(filename, "FolderSource", textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            TamPubWin1.Etc.loadDesktop(TamPub1.FileOperation.currentFilePath + "config.xml", this);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            TamPubWin1.Etc.saveDesktop(TamPub1.FileOperation.currentFilePath + "config.xml", this);
+            task.exit = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+            TamPub1.ConfigFileXml.writeString(filename, "FolderBackup", textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+            TamPub1.ConfigFileXml.writeString(filename, "repeatInterval", textBox4.Text);
+        }
+
+        private void saveNoprocessList() {
+            string filename = TamPub1.FileOperation.currentFilePath + "config.xml";
+        }
+    }
+}
+
